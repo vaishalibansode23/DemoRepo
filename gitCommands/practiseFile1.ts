1caf67c6bb0c37b7890eb839c67e9a1bfeeaf885
vaishalibansode23 <bansodevaishali321@gmail.com>
2025-11-14 14:49:08 +0530

    adding new console log line

--- gitCommands/practiseFile1.ts
+++ gitCommands/practiseFile1.ts
@@ -1,7 +1,8 @@
 let arr=[1,2,3,4,5];
 console.log("Array elements are:");
 for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
+    //console.log(arr[i]);
+    console.log(`Element at index ${i} is ${arr[i]}`);
 } 
 
 let sum=arr.reduce((a,b)=>a+b,0);
