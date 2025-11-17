e2ec4cb2073d4dcab20cf6f174e566d9a8c09731
vaishalibansode23 <bansodevaishali321@gmail.com>
2025-11-17 10:48:59 +0530

    add hello second time

--- gitCommands/practiseFile1.ts
+++ gitCommands/practiseFile1.ts
@@ -1,6 +1,7 @@
 let arr=[1,2,3,4,5];
 console.log("Array elements are:");
 for(let i=0;i<arr.length;i++){
+    console.log(arr[i],"hello");
     console.log(`Element at index ${i} is ${arr[i]}`);
 } 
 
